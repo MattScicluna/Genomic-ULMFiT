d1316edd6e5ae8a1bc79f14f0df80d5dcb905c8c
Matthew Scicluna <sciclun4@beluga3.int.ets1.calculquebec.ca>
2020-07-17 15:59:35 -0400

    fixed bacteria names file

--- Bacteria/download_bact_fastas.sh
+++ Bacteria/download_bact_fastas.sh
@@ -5,5 +5,5 @@ input="bacterias.txt"
 while IFS=" " read -r id file
 do
   echo "downloading id = $id into $file"
-  #esearch -db nucleotide -query $id | efetch -format fasta > "$file"
+  esearch -db nucleotide -query $id | efetch -format fasta > "/lustre04/scratch/sciclun4/genome_data/genomic_ulmfit/bacterial_genomes/genome_fastas/${file}"
 done < "$input"
